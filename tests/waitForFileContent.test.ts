103bb2f1071995888d4fc2906c761545b6cd0b73
neverland <chenjiahan.jait@bytedance.com>
2026-07-13 10:44:55 +0800

    feat: add tests for waitForFile

---
+++ tests/waitForFileContent.test.ts
@@ -0,0 +1,28 @@
+import { mkdtemp, readFile, rm, writeFile } from 'node:fs/promises';
+import { tmpdir } from 'node:os';
+import { join } from 'node:path';
+import { setTimeout as delay } from 'node:timers/promises';
+import { expect, test } from 'rstack/test';
+import { waitForFileContent } from '../src/index';
+
+test('should wait until a file includes the expected content', async () => {
+  const directoryPath = await mkdtemp(join(tmpdir(), 'rstack-test-utils-'));
+  const filePath = join(directoryPath, 'index.js');
+
+  try {
+    const writePromise = delay(10).then(() =>
+      writeFile(filePath, 'updated content'),
+    );
+
+    await Promise.all([
+      waitForFileContent(filePath, 'updated', {
+        interval: 1,
+        timeout: 1000,
+      }),
+      writePromise,
+    ]);
+    expect(await readFile(filePath, 'utf-8')).toBe('updated content');
+  } finally {
+    await rm(directoryPath, { force: true, recursive: true });
+  }
+});
