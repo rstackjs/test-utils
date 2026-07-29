5d12f8975f51322d724d4680c8045f8dba1da67f
neverland <chenjiahan.jait@bytedance.com>
2026-07-29 13:14:51 +0800

    chore: upgrade Rstack CLI to v0.2.0

--- rstack.config.ts
+++ rstack.config.ts
@@ -8,3 +8,12 @@ define.lint(async () => {
   const { js, ts } = await import('rstack/lint');
   return [js.configs.recommended, ts.configs.recommended];
 });
+
+define.staged({
+  '*.{md,mdx,json,css,less,scss}':
+    'prettier --write --no-error-on-unmatched-pattern',
+  '*.{js,jsx,ts,tsx,mjs,cjs}': [
+    'rs lint --type-check',
+    'prettier --write --no-error-on-unmatched-pattern',
+  ],
+});
